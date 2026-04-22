c70e959ce81ca43ea3a1626df44a0ec63494e0f1
Elizabeth <elizabethteresa1717@gmail.com>
2026-04-21 21:59:34 -0700

    deleted everything related to OPENAI AND SCANNER

--- backend/routes/convertMenuRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router({ mergeParams: true });
-
-const { convertMenu } = require("../controllers/convertMenuController");
-
-// POST /api/:client/convert-menu
-router.post("/:client/convert-menu", convertMenu);
-
-module.exports = router;
