45fcfeb4f622507bde05c24049e592bc9955a8fe
Elizabeth Teresa-Ramirez <elizabethteresa1717@gmail.com>
2026-07-31 10:21:15 -0400

    changed schedule time

--- backend/cron/reminderCron.js
+++ backend/cron/reminderCron.js
@@ -2,7 +2,7 @@ const cron = require("node-cron");
 const { sendReminders } = require("../controllers/snackListController");
 
 cron.schedule(
-  "10 14 * * *",
+  "00 11 * * *",
   async () => {
     console.log("⏰ Running daily snack reminder job...");
 
